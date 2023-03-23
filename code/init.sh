cc1aa8c79e2412d36fe8d3ea8813a225483601b6
ronaldas.macas <ronaldas.macas@ligo.org>
2023-03-23 07:52:07 -0700

    updated ini file to work with dqr tasks

--- code/init.sh
+++ code/init.sh
@@ -1,12 +1,10 @@
 python eval.py \
-    --verbose \
-    --git_dir /home/jdo86av3c/projects/eval \
+    --git_dir /home/ronaldas.macas/projects/eval \
     --events_file events_testing.csv \
     --contact_file contacts_testing.csv \
     --md_file table_testing.md \
     --vol_file rota_testing.csv \
     --dqr_url https://dqr.updateMe.com \
     --eval_url https://dqr.updateMe.com \
-    --validator 'Liz Truss' \
-    --far 0.5 \
+    --ignore_far \
     --json G361581.json

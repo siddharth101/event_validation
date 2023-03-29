e56a24f200c12c028fe8094855252ccc372746c2
ronaldas.macas <ronaldas.macas@ligo.org>
2023-03-29 03:25:16 -0700

    adding files for packaging

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from eval import eval
+from app import app
+from utils import utils
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='eval',
+    version='0.1',
+    author='Ronaldas Macas',
+    author_email='ronaldas.macas@ligo.org',
+    description='Event validation infrastructure',
+    url='https://git.ligo.org/detchar/event-validation/',
+    project_urls = {
+        "Bug Tracker": "https://git.ligo.org/detchar/event-validation/issues"
+    },
+    license='MIT',
+    packages=['eval'],
+    install_requires=['numpy', 'scipy', 'gwpy', 'matplotlib', 'astropy', 'pandas', 'mdutils', 'markdown-include', 'mkdocs-material-igwn', 'tabulate', 'flask', 'Flask-WTF', 'Flask-Bootstrap4', 'Flask-Table', 'gunicorn'],
+)

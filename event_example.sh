80416fb65f240309647e2967c2660b0e21f2d611
Ronaldas Macas <ronaldas.macas@port.ac.uk>
2023-04-28 11:38:02 +0100

    updating example to be compatible with the newest EV version

--- event_example.sh
+++ event_example.sh
@@ -2,7 +2,6 @@ python event_validation/create_event.py \
     --verbose \
     --git_dir . \
     --events_file events_testing.csv \
-    --contact_file contacts_testing.csv \
     --md_file table_testing.md \
     --vol_file rota_testing.csv \
     --dqr_url https://dqr.updateMe.com \

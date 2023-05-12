531a0b85d98ddf408638336eb6043d5af6d603bf
Ronaldas Macas <ronaldas.macas@port.ac.uk>
2023-05-12 10:01:41 +0100

    updating default docs url

--- event_validation/create_event.py
+++ event_validation/create_event.py
@@ -216,7 +216,7 @@ def main():
     parser.add_argument('--dqr_url', type=str, required=True, help='Data quality report URL')
     parser.add_argument('--eval_url', type=str, default='https://dqr.ligo.caltech.edu/ev_forms', help='Event validation form URL, default: https://dqr.ligo.caltech.edu/ev_forms')
     parser.add_argument('--gitlab_url', type=str, default='https://git.ligo.org/detchar/event-validation/-/issues', help='GitLab issues URL, default: https://git.ligo.org/detchar/event-validation/-/issues')
-    parser.add_argument('--docs_url', type=str, default='https://ldas-jobs.ligo.caltech.edu/~ronaldas.macas/eval_website/', help='Documentation URL, default: https://ldas-jobs.ligo.caltech.edu/~ronaldas.macas/eval_website/')
+    parser.add_argument('--docs_url', type=str, default='https://ldas-jobs.ligo.caltech.edu/~dqr/event_validation', help='Documentation URL, default: https://ldas-jobs.ligo.caltech.edu/~dqr/event_validation')
     parser.add_argument('--send_email', action=argparse.BooleanOptionalAction, help='Send notification emails.')
     parser.add_argument('--create_issue', action=argparse.BooleanOptionalAction, help='Create a git issue for the candidate event')
     args = parser.parse_args()
